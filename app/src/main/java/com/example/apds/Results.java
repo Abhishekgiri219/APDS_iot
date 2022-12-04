b698bf84ca7957bc35e235683a4adc042d1a0228
Abhishek Giri <abhishekgiri219@gmail.com>
2022-12-04 20:29:02 +0530

    Ml Api hooked

---
+++ app/src/main/java/com/example/apds/Results.java
@@ -0,0 +1,17 @@
+package com.example.apds;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Results {
+    @SerializedName("result")
+    private String result;
+
+
+    public Results(String result) {
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
